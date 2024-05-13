a09d24358939f62300b545b62db6a4a8b3781376
MagdalenaVasylyshyn <141566623+MagdalenaVasylyshyn@users.noreply.github.com>
2024-05-13 17:08:29 +0300

    First version of fourth homework

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import mongoose from "mongoose";
-import "dotenv/config";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
-import contactsRouter from "./routes/contactsRouter.js";
-
-const DB_URI = process.env.DB_URI;
+import contactsRouter from './routes/contactsRouter.js';
+import authRouter from './routes/authRouter.js';
 
 const app = express();
 
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use('/users', authRouter);
+app.use('/api/contacts', contactsRouter);
 
 app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+  res.status(404).json({ message: 'Route not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
+  const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
 
+const uri = process.env.DB_HOST;
+const port = process.env.PORT || 8000;
+
+// run server
 (async () => {
   try {
-    await mongoose.connect(DB_URI);
+    await mongoose.connect(uri);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log('Database connection successful');
 
-    app.listen(3000, () => {
-      console.log("Server is running. Use our API on port: 3000");
+    app.listen(port, () => {
+      console.log(`Server is running. Use our API on port: ${port}`);
     });
   } catch (error) {
     console.error(error);
